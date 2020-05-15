a5a9ff08925a1163dbc5b15df51ae1de26c4859c
areebadroit <mdareebansar@gmail.com>
2020-05-16 05:10:11 +0530

    version 2

--- v1/index.js
+++ v1/index.js
@@ -6,10 +6,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-    {name: "Camp Exotica", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e5074417c2c7cd5964fcc_340.jpg"},
-    {name: "Rishikesh Valley camp", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417c2c7cd5964fcc_340.jpg"},
-    {name: "Kipling Camp", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf85254794076287ad59544_340.jpg"},
-    {name: "West Ladakh Camp", image: "https://pixabay.com/get/54e5dc474355a914f1dc84609620367d1c3ed9e04e5074417c2c7cd5964fcc_340.jpg"}
+    {name: "Camp Exotica", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60.jpgps://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e5074417c2c7cd5964fcc_340.jpg"},
+    {name: "Rishikesh Valley camp", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60.jpg"},
+    {name: "Kipling Camp", image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60.jpg"},
+    {name: "West Ladakh Camp", image: "https://images.unsplash.com/photo-1534187886935-1e1236e856c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60.jpg"}
 ];
 
 app.get("/", (req, res) => {
